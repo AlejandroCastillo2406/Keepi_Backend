e4f1432b87482aa5cc62310c66474eecef91e114
thistan123 <thistan08@gmail.com>
2026-03-24 11:00:47 -0600

    cambio de martes

--- app/repositories/archivo_repository.py
+++ app/repositories/archivo_repository.py
@@ -1,22 +1,31 @@
+from sqlalchemy import text
+
 class ArchivoRepository:
 
     def __init__(self, db):
         self.db = db
 
     def guardar(self, nombre, ruta, usuario_id, token, expiracion):
-        cursor = self.db.cursor()
-        cursor.execute("""
-            INSERT INTO ARCHIVOS_TEMPORALES 
+        self.db.execute(text("""
+            INSERT INTO archivos_temporales 
             (nombre_archivo, ruta_archivo, usuario_id, token_acceso, fecha_expiracion)
-            VALUES (?, ?, ?, ?, ?)
-        """, (nombre, ruta, usuario_id, token, expiracion))
+            VALUES (:nombre, :ruta, :usuario_id, :token, :expiracion)
+        """), {
+            "nombre": nombre,
+            "ruta": ruta,
+            "usuario_id": usuario_id,
+            "token": token,
+            "expiracion": expiracion
+        })
+
         self.db.commit()
 
     def obtener_por_token(self, token):
-        cursor = self.db.cursor()
-        cursor.execute("""
-            SELECT ruta_archivo, usuario_id, fecha_expiracion
-            FROM ARCHIVOS_TEMPORALES
-            WHERE token_acceso = ?
-        """, (token,))
-        return cursor.fetchone()
+        result = self.db.execute(text("""
+            SELECT ruta_archivo 
+            FROM archivos_temporales
+            WHERE token_acceso = :token
+        """), {"token": token})
+
+        row = result.fetchone()
+        return row[0] if row else None
